2ff5c4c680e2ab07ed861fd10ccba9bf7095f4e3
Hiob <hello@hiob.fr>
2022-11-09 14:54:07 +0000

    removing www-data from sudoers

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -4,11 +4,14 @@
 ## Add hostname
 sudo bash -c "echo '$(hostname -i) $(hostname) $(hostname).localhost' >> /etc/hosts"
 
-## Laucnh Sendmail
+## Launch Sendmail service
 sudo service sendmail start
 
 # Remove Apache pre-existing PID files
 sudo rm -f /var/run/apache2/apache2.pid
+
+# Removing www-data from sudoers
+sudo deluser www-data sudo
 
 # Start Apache (CMD)
 exec "$@"
